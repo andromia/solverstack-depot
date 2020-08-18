e66ceae463224cae15d1cba34c08bdc09bc26e6d
chris pryer <christophpryer@gmail.com>
2020-08-17 21:54:40 -0400

    added request test

--- tests/test_origin.py
+++ tests/test_origin.py
@@ -1,6 +1,36 @@
 from . import common
-from app import origin
+from app import origin, create_app
+from app import __version__
 
+from config import Config
+
+import logging
+import pytest
+import json
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+
+@pytest.fixture
+def client():
+    yield create_app(TestConfig).test_client()
+    
+
+def test_main_procedure(client):
+    input_data = common.DATA
+    logging.debug(f'input data : {input_data}')
+
+    endpoint = f'/api/{__version__}/origin'
+    logging.debug(f'endpoint: {endpoint}')
+
+    response = client.post(endpoint, json=input_data)
+    output = json.loads(response.get_data())
+
+    assert len(output) == 2
+    assert all(ele in ["latitude", "longitude"] for ele in output)
 
 def test_geocode():
     lats = common.TESTING_CSV_DF.latitude.tolist()
